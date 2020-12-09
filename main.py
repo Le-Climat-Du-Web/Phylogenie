7ad344673ceb0e5431600467e3e8cbeec47d17cc
Le-Climat-Du-Web <75421008+Le-Climat-Du-Web@users.noreply.github.com>
2020-12-09 14:49:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -13,41 +13,34 @@ import matplotlib
 matplotlib.use('Agg')
 
 
-
-
-
 def get_random_string(length):
     """Générer une chaîne aléatoire de longueur fixe"""
     str = string.ascii_lowercase
     return ''.join(random.choice(str) for i in range(length))
 
 
-
-
 def get_fasta(id_list):
     # Create a random name for each user session and create corresponding directories in order to allow multiple simultaneous uses without loss of data
-    dirName = get_random_string(10) + "/"
-    saveDir = "static/data/sauvegardes/"
+    dirName = get_random_string(10) + "/" 
     os.makedirs(saveDir + dirName, exist_ok=True)
     # write a fasta file for each gene
     filename_list = []
     for gene_id in id_list:
         records = Entrez.efetch(db="nucleotide", id=gene_id, rettype="fasta", retmode="text")
         filename = (gene_id + '.fasta').format(records)
-        filename_list.append("static/data/sauvegardes/" + dirName + filename)
+        filename_list.append(saveDir + dirName + filename)
         print('Writing:{}'.format(filename))
         time.sleep(1)
-        with open('static/data/sauvegardes/' + dirName + filename, 'w') as f:
+        with open(saveDir + dirName + filename, 'w') as f:
             f.write(records.read())
             f.close()
 
     # combine files in multifasta file
-    with open('static/data/sauvegardes/'+ dirName + 'multifasta.fasta', 'w') as outfile:
+    with open(saveDir+ dirName + 'multifasta.fasta', 'w') as outfile:
         for fname in filename_list:
             with open(fname) as infile:
                 outfile.write(infile.read())
     return dirName
-
 
 
 def clustal_alignment(infile, outfile):
@@ -56,13 +49,12 @@ def clustal_alignment(infile, outfile):
         clustal_exe = "static/tools/MacOS/clustal-omega-1.2.3-macosx"
     if (user_OS == 'linux'):
         clustal_exe = "static/tools/Linux/clustalo-1.2.4-Ubuntu-x86_64"
-    if (user_OS == 'win32'):
-        clustal_exe = current_path + "/static/tools/Windows/clustal-omega-1.2.2-win64/clustalo.exe"
+    if (user_OS == 'win64'):
+        clustal_exe = current_path + "static/tools/Windows/clustal-omega-1.2.2-win64/clustalo.exe"
 
-    cline = ClustalwCommandline(clustal_exe, infile="static/data/sauvegardes/" + dirName + infile,
-                                outfile="static/data/sauvegardes/" + dirName + outfile)
+    cline = ClustalwCommandline(clustal_exe, infile= saveDir + dirName + infile,
+                                outfile= saveDir + dirName + outfile)
     stdout, stderr = cline()
-
 
 
 def muscle_alignment(infile, outfile):
@@ -71,18 +63,18 @@ def muscle_alignment(infile, outfile):
         muscle_exe = "static/tools/MacOS/muscle3.8.31_i86darwin64"
     if(user_OS == 'linux'):
         muscle_exe = "static/tools/Linux/muscle3.8.31_i86linux64"
-    if(user_OS == 'win32'):
-        muscle_exe = current_path + "/static/tools/Windows/muscle3.8.31_i86win32.exe"
+    if(user_OS == 'win64'):
+        muscle_exe = current_path + "static/tools/Windows/muscle3.8.31_i86win32.exe"
 
-    in_file = "static/data/sauvegardes/" + dirName + infile
-    out_file = "static/data/sauvegardes/" + dirName + outfile
+    in_file = saveDir + dirName + infile
+    out_file = saveDir + dirName + outfile
     muscle_cline = MuscleCommandline(muscle_exe, input=in_file, out=out_file)
     stdout, stderr = muscle_cline()
 
 
 def NJ_tree(infile, file_type):
     #Tree creation with neighbor-joining
-    filename = "static/data/sauvegardes/" + dirName + infile
+    filename = saveDir + dirName + infile
     aln = AlignIO.read(filename, file_type) #clustal si alignement clustal, fasta si alignement fasta
     calculator = DistanceCalculator('identity')
     constructor = DistanceTreeConstructor(calculator, 'nj') # nj ou UPGMA
@@ -92,8 +84,8 @@ def NJ_tree(infile, file_type):
     #Phylo.draw_ascii(tree)
     tree.ladderize()
     Phylo.draw(tree, do_show=False)
-    Phylo.write(tree, 'static/data/sauvegardes/' + dirName + 'tree.txt', "newick")
-    foo = current_path + "/static/data/sauvegardes/" + dirName + 'tree.png'
+    Phylo.write(tree, saveDir + dirName + 'tree.txt', "newick")
+    foo = current_path + saveDir + dirName + 'tree.png'
     plt.savefig(foo)
 
 
@@ -104,8 +96,8 @@ def ML_tree(infile, outfile, file_type):
     # phylogeny page should allow to choose maximum likelihood method
 
     # convert file to phylip
-    records = SeqIO.parse("static/data/sauvegardes/" + dirName + infile, file_type)  # clustal <-> fasta
-    count = SeqIO.write(records, "static/data/sauvegardes/" + dirName + outfile + ".phylip", "phylip")
+    records = SeqIO.parse(saveDir + dirName + infile, file_type)  # clustal <-> fasta
+    count = SeqIO.write(records, saveDir + dirName + outfile + ".phylip", "phylip")
     print("Converted %i records" % count)
 
     if (user_OS == 'darwin'):
@@ -114,15 +106,15 @@ def ML_tree(infile, outfile, file_type):
     if (user_OS == 'linux'):
         cmd = PhymlCommandline(cmd='static/tools/Linux/PhyML-3.1/PhyML-3.1_linux64',
                                input='static/data/sauvegardes/' + dirName + outfile + '.phylip')
-    if (user_OS == 'win32'):
-        cmd = PhymlCommandline(cmd= current_path + '/static/tools/Windows/PhyML-3.1/PhyML-3.1_win32.exe',
-                               input='static/data/sauvegardes/' + dirName + outfile + '.phylip')
+    if (user_OS == 'win64'):
+        cmd = PhymlCommandline(cmd= current_path + 'static/tools/Windows/PhyML-3.1/PhyML-3.1_win32.exe',
+                               input= saveDir + dirName + outfile + '.phylip')
 
     out_log, err_log = cmd()
-    tree = Phylo.read('static/data/sauvegardes/' + dirName + outfile + '.phylip_phyml_tree.txt', 'newick')
+    tree = Phylo.read(saveDir + dirName + outfile + '.phylip_phyml_tree.txt', 'newick')
     Phylo.draw(tree, do_show=False)
-    Phylo.write(tree, 'static/data/sauvegardes/' + dirName + 'tree.txt', "newick")
-    foo = current_path + '/static/data/sauvegardes/' + dirName + 'tree.png'
+    Phylo.write(tree, saveDir + dirName + 'tree.txt', "newick")
+    foo = current_path + saveDir + dirName + 'tree.png'
     plt.savefig(foo)
 
 
@@ -132,7 +124,7 @@ def ML_tree(infile, outfile, file_type):
 Entrez.email = "sentinelles.bioinfo@gmail.com"
 
 # Define user's operating system
-user_OS = sys.platform
+user_OS = 'win64'
 
 current_path = os.path.dirname(__file__)
 parent_path = os.path.dirname(current_path)
@@ -142,6 +134,7 @@ id_list = ["AY158636.1","AY158639.1","AY159811.1","AY159808.1","AY159809.1","AY1
 name_gene = ["Vipera berus Pla2Vb", "Vipera berus AmtI2", "Vipera berus AmtI1", "Vipera aspis AmtI1", "Vipera aspis AmtI1", "Vipera aspis (AmtI2)", "Vipera aspis zinnikeri AmtI1"]
 
 dirName=""
+saveDir = "C:/wamp64/www/Phylogenie/static/data/sauvegardes/"
 
 ## Function calls to use the program in the terminal without the user interface
 #dirName = get_fasta(id_list)
