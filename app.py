cc0b8954b019623e1c16f0ce8d677889d6fd177b
Julie PRATX <58253085+Erufen@users.noreply.github.com>
2020-12-13 10:09:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,36 +2,24 @@ from flask import Flask, render_template, request
 from datetime import timedelta
 import main
 
-
-
 app = Flask(__name__)
-
-
 
 app.config['DEBUG'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
-
-
 
 @app.route('/')
 def mainAcceuil():
     return render_template('Acceuil.html', selectedMenu="Accueil")
 
-
-
 @app.route('/infos')
 def infos_caract():
     return render_template('infos.html', selectedMenu="Infos")
-
-
 
 @app.route('/bdd')
 def geneId():
     res = main.id_list
     names = main.name_gene
     return render_template('bdd.html', idlist=res,namegene=names, selectedMenu="Base de données")
-
-
 
 @app.route('/bdd',methods=['GET','POST'])
 def choixGene():
@@ -41,8 +29,6 @@ def choixGene():
             main.dirName = main.get_fasta(id_list)
     res=main.dirName
     return render_template('Alignement.html', res=res, selectedMenu="Alignement")
-
-
 
 @app.route('/Alignement', methods=['POST'])
 def Alignement():
@@ -60,7 +46,6 @@ def Alignement():
             main.ML_tree("obtenu.fasta", "msa_muscle", type)
         res=main.dirName
         return render_template('tree.html', res=res, selectedMenu="Phylogénie")
-
 
 
 if __name__ == '__main__':
